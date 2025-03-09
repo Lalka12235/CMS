8c649c79b0a96e09200c2b45f84f7205ba4ed2fb
Egor <61911209+Lalka12235@users.noreply.github.com>
2025-03-09 15:25:47 +0700

    Update article_model.py

--- app/data/article_model.py
+++ app/data/article_model.py
@@ -2,11 +2,17 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class Article(BaseModel):
+    id: int
     title: str
-    author: str
+    username: str
     description: str
-    data: datetime
+    created_at: datetime
+    updated_at: datetime | None = None
 
 class User(BaseModel):
-    username:str
-    blogs: Article
+    username: str
+    blogs: list[Article] = []
+    is_admin: bool = False
+
+class ADmin(User):
+    is_admin: bool = True
